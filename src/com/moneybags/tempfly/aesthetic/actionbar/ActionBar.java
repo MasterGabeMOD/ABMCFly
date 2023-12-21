8b30b0dd66f904567eaafd7a0951ca5e1c9aa745
MasterGabeMOD <MasterGabeMod@yahoo.com>
2023-12-20 16:41:39 -0800

    Initial commit

---
+++ src/com/moneybags/tempfly/aesthetic/actionbar/ActionBar.java
@@ -0,0 +1,37 @@
+package com.moneybags.tempfly.aesthetic.actionbar;
+
+import com.moneybags.tempfly.TempFly;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public abstract class ActionBar {
+    protected final TempFly tempfly;
+
+    public ActionBar(TempFly tempfly) {
+        this.tempfly = tempfly;
+    }
+
+    public abstract void sendActionBar(final Player player, final String message);
+
+    public void sendActionBar(final Player player, final String message, int duration) {
+        sendActionBar(player, message);
+
+        if (duration >= 0) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, "");
+                }
+            }.runTaskLater(tempfly, duration + 1);
+        }
+
+        if (duration > 0) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, message);
+                }
+            }.runTaskTimer(tempfly, 40L, 40L);
+        }
+    }
+}
