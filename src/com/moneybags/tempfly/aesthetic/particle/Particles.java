42a46ddaa811b501e93193b24ab0ec7db709400a
MasterGabeMOD <MasterGabeMod@yahoo.com>
2024-01-17 02:44:14 -0800

    Really try to improve performance a bit more.
    Particles are difficult to optimize, but after some further testing, this seems sufficient.

--- src/com/moneybags/tempfly/aesthetic/particle/Particles.java
+++ src/com/moneybags/tempfly/aesthetic/particle/Particles.java
@@ -1,8 +1,5 @@
 package com.moneybags.tempfly.aesthetic.particle;
 
-import java.util.Random;
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Effect;
@@ -19,12 +16,15 @@ import com.moneybags.tempfly.util.V;
 import com.moneybags.tempfly.util.data.DataBridge.DataValue;
 import com.moneybags.tempfly.util.data.DataPointer;
 
+import java.util.Random;
+import java.util.UUID;
+
 public class Particles {
 
-    private static Class<?> dustOptions = Particle.DustOptions.class;
-    private static Class<?> blockData = org.bukkit.block.data.BlockData.class;
+    private static final Random rand = new Random();
     private static TempFly tempfly;
     private static boolean oldParticles;
+    private static final String DEFAULT_PARTICLE = "HAPPY_VILLAGER";
 
     public static void initialize(TempFly plugin) {
         tempfly = plugin;
@@ -37,9 +37,7 @@ public class Particles {
         return majorVersion <= 8; // 1.8 or below 
     }
 
-    private static Random rand = new Random();
-
-    public static void playAsync(final Location loc, final String s) {
+    public void playAsync(final Location loc, final String s) {
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -48,25 +46,24 @@ public class Particles {
         }.runTaskAsynchronously(tempfly);
     }
 
+    private static Particle switchParticle(String s) {
+        try {
+            return Particle.valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return Particle.VILLAGER_HAPPY;
+        }
+    }
+
     public static void play(Location loc, String s) {
         if (!oldParticles) {
-            Particle particle = null;
-            try {
-                particle = Particle.valueOf(s.toUpperCase());
-            } catch (IllegalArgumentException e1) {
-                try {
-                    particle = Particle.valueOf(V.particleType.toUpperCase());
-                } catch (IllegalArgumentException e2) {
-                    particle = Particle.VILLAGER_HAPPY;
-                }
-            }
+            Particle particle = switchParticle(s != null && s.equalsIgnoreCase("ITEM_BREAK") ? "HAPPY_VILLAGER" : s);
 
             Class<?> c = particle.getDataType();
             try {
-                if (dustOptions.equals(c)) {
-                    loc.getWorld().spawnParticle(particle, loc, 1,
-                            new DustOptions(Color.fromRGB(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)), 2f));
-                } else if (blockData.equals(c)) {
+                if (Particle.DustOptions.class.equals(c)) {
+                    Color[] colors = {Color.fromRGB(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255))};
+                    loc.getWorld().spawnParticle(particle, loc, 1, new DustOptions(colors[rand.nextInt(colors.length)], 2f));
+                } else if (org.bukkit.block.data.BlockData.class.equals(c)) {
                     loc.getWorld().spawnParticle(particle, loc, 1, Material.STONE.createBlockData());
                 } else {
                     loc.getWorld().spawnParticle(particle, loc, 1, 0, 0, 0, 0.1);
@@ -75,20 +72,16 @@ public class Particles {
                 loc.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, loc, 1, 0, 0, 0, 0.1);
             }
         } else {
-            Effect particle = null;
-            if (s != null && s.equalsIgnoreCase("ITEM_BREAK")) {
-                s = "HAPPY_VILLAGER";
-            }
-            try {
-                particle = Effect.valueOf(s.toUpperCase());
-            } catch (IllegalArgumentException e1) {
-                try {
-                    particle = Effect.valueOf(V.particleType);
-                } catch (IllegalArgumentException e2) {
-                    particle = Effect.valueOf("HAPPY_VILLAGER");
-                }
-            }
+            Effect particle = switchEffect(s != null && s.equalsIgnoreCase("ITEM_BREAK") ? "HAPPY_VILLAGER" : s);
             loc.getWorld().playEffect(loc, particle, 1);
+        }
+    }
+
+    private static Effect switchEffect(String s) {
+        try {
+            return Effect.valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return Effect.valueOf(DEFAULT_PARTICLE);
         }
     }
 
@@ -112,5 +105,4 @@ public class Particles {
             tempfly.getDataBridge().stageChange(DataPointer.of(DataValue.PLAYER_TRAIL, u.toString()), particle);
         }
     }
-
 }
